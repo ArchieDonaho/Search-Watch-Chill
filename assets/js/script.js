2c880fa995f1d44136b902d9146d9b7932ec93a4
AD <archied5150@yahoo.com>
2022-03-15 19:08:52 -0500

    ability to get movie data from name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
-// fetch("https://api.themoviedb.org/3/movie/550?api_key=983c87bc5226584d6913b9818f37ade3");
+var getMovieId = function(movieName){
+    var link = "https://api.themoviedb.org/3/search/movie?api_key=983c87bc5226584d6913b9818f37ade3&query=" + movieName;
 
-// " + movie + "?api_key=983c87bc5226584d6913b9818f37ade3
-var getMovie = function(movieId){
+    fetch(link).then(function(response){
+        //if we get a 2XX status code
+        if(response.ok){
+            response.json().then(function(data){
+
+                console.log(data);
+
+                getMovieDetails(data.results[0].id);
+
+            })
+        } else {
+            alert("Please Try Again")
+        }
+    })
+}
+
+var getMovieDetails = function(movieId){
     var link = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=983c87bc5226584d6913b9818f37ade3"; 
 
     fetch(link).then(function(response){
@@ -17,8 +33,8 @@ var getMovie = function(movieId){
         }
     })
 }
-//shre = 808
-var movie = "808";
+
+var movie = "shrek";
 
 
-getMovie(movie);
+getMovieId(movie);
